4acccd7b42999dac6de1f6badd2dd5c420c41374
Grant Kemp <grant@kempnet.com>
2015-09-15 22:46:48 +0100

    updated Api Key

--- VirtualTourist/VirtualTourist/VirtualTourist/FlickrConfig.swift
+++ VirtualTourist/VirtualTourist/VirtualTourist/FlickrConfig.swift
@@ -11,8 +11,7 @@ import Foundation
 class FlickrConfig: NSObject {
     
     static var authUrl = "https://www.flickr.com/auth-72157657554563956"
-    static var apikey = "3dc7c70110d1b98201787efbb59df4d7"
-    static var secret = "4787ee9e52ce4e46" //TODO: Remove this if not needed due to security
+    static var apikey = "1448f15c8e6659546bcdfa900c6caec4"
     static var numToShowPerPage = 4
     static var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
     static var nameOfCollection = "default"
